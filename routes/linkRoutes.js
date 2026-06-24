5402a5f99578eaf1c6f3feba35496f99498ca07b
gurdeepsdev <gurdeep@clickorbits.com>
2026-06-24 17:59:17 +0530

    Update linkRoutes.js

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -5,6 +5,9 @@ const controller = require("../controllers/linkController");
 router.post("/advertiser", controller.saveAdvertiserLink);
 router.post("/publisher", controller.generatePublisherLink);
 router.post("/phandle", controller.handlePostback);
+router.post("/publisher/disapprove", controller.disapprovePublisher); 
+router.get("/publisher", controller.getPublisherLinks);
+router.get("/publisher-api-url", controller.getPublisherApiUrl);
 
 
 
